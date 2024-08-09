64a1c2434c7f467fe23b6135d5eebfba8870a745
Verevka <verevka86@gmail.com>
2024-08-09 09:13:17 +0300

    Fix custom vendor overlay partition mount

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-# Custom Partitions mounted
-modem_part=/data/.stowaways/firmware/modem.img
-dsp_part=/data/.stowaways/firmware/dsp.img
-bluetooth_part=/data/.stowaways/firmware/bluetooth.img
-
-mount -v -o loop,ro,shortname=lower,uid=1000,gid=1000,dmask=007,fmask=007 -t vfat $modem_part  /vendor/firmware_mnt
-mount -v -o loop,ro,nosuid,nodev,barrier=1 -t ext4 $dsp_part /vendor/dsp
-mount -v -o loop,ro,shortname=lower,uid=1002,gid=3002,dmask=227,fmask=337 -t vfat $bluetooth_part /vendor/bt_firmware
-
 echo -1000 > /proc/self/oom_score_adj
