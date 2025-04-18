3d3d881bb1c6980281b353229e36be718e5b0eb0
Verevka <verevka86@gmail.com>
2025-04-18 09:55:54 +0300

    bind audio.hidl_compat.default.so

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -3,4 +3,6 @@
 # This should prevent `echo 1 > /dev/ipa` to crash the kernel
 echo -n "/vendor/firmware" > /sys/module/firmware_class/parameters/path
 
+mount --bind /usr/libexec/droid-hybris/system/lib64/hw/audio.hidl_compat.default.so /vendor/lib64/hw/audio.primary.kona.so
+
 echo -1000 > /proc/self/oom_score_adj
